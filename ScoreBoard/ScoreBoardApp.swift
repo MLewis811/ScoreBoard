d486fb70e091e6804f2af96b1e75591f6cfecdae
Mike Lewis <revmikelewis@gmail.com>
2025-10-17 11:52:55 -0500

    Initial Commit

---
+++ ScoreBoard/ScoreBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreBoardApp.swift
+//  ScoreBoard
+//
+//  Created by Mike Lewis on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
